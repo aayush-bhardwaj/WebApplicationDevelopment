6edaa3e57fa1cee21224c6e5925395c236b68f31
aayush-bhardwaj <aayush.bhj@gmail.com>
2016-09-05 19:16:36 +0530

    Done with the Front-end coding

--- Chirp/javascript/chirpApp.js
+++ Chirp/javascript/chirpApp.js
@@ -10,4 +10,17 @@ app.controller('mainController',function($scope){
 		$scope.newPost = {'created_by':'', 'text':'', 'created_at':''};
 	};
 	
-})
+});
+
+app.controller('authController',function($scope){
+	$scope.user = {'username':'' , 'password':''};
+	$scope.error_message='';
+
+	$scope.login = function(){
+		$scope.error_message = 'Registration request for ' + $scope.user.username;
+	};
+
+	$scope.register = function(){
+		$scope.error_message = 'Login request for ' + $scope.user.username;
+	};
+});
